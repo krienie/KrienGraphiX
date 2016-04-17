098c82a6fa86f8331aa9eff274dd825855a3e7ba
Krien Linnenbank <krien.linnenbank@ziggo.nl>
2016-04-18 00:04:36 +0200

    removed "using namespace qi" from OBJParser; fixed small parsing data processing bug where duplicate vertex indices were stored

--- KGConverter/OBJParser.cpp
+++ KGConverter/OBJParser.cpp
@@ -6,12 +6,14 @@
 #include <fstream>
 #include <map>
 
+#include <boost/filesystem.hpp>
+
 #ifndef BOOST_SPIRIT_USE_PHOENIX_V3
 #define BOOST_SPIRIT_USE_PHOENIX_V3
 #endif
-#include "boost/spirit/include/qi.hpp"
-#include "boost/spirit/include/phoenix.hpp"
-#include "boost/spirit/include/support_iso8859_1.hpp"
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+#include <boost/spirit/include/support_iso8859_1.hpp>
 
 #include "OBJParser.h"
 
@@ -22,10 +24,10 @@ BOOST_FUSION_ADAPT_STRUCT(
 	(float, z)
 );
 
+namespace bfs     = boost::filesystem;
 namespace phx     = boost::phoenix;
 namespace qi      = boost::spirit::qi;
 namespace iso8859 = boost::spirit::iso8859_1;
-using namespace qi;
 typedef qi::rule<std::string::const_iterator> Skipper;
 
 namespace kgx
@@ -152,34 +154,34 @@ namespace kgx
 		{
 			ObjGram() : ObjGram::base_type(start)
 			{
-				skipLine = ((lit('#') | lit('s') | lit('o')) >> qi::skip(qi::blank)[*qi::print]);
-				defaultGroup = lit('g') >> qi::skip(qi::blank)[*qi::print];
+				skipLine = ((qi::lit('#') | qi::lit('s') | qi::lit('o')) >> qi::skip(qi::blank)[*qi::print]);
+				defaultGroup = qi::lit('g') >> qi::skip(qi::blank)[*qi::print];
 
-				auto setMatlib = phx::push_back(phx::bind(&ObjParseData::mtllib, _r1), qi::_1);
+				auto setMatlib = phx::push_back(phx::bind(&ObjParseData::mtllib, qi::_r1), qi::_1);
 				matlib = "mtllib" >> qi::skip(qi::blank)[*qi::print[setMatlib]];
-				vertex = -defaultGroup >> 'v' >> repeat(3)[double_[phx::push_back(phx::bind(&ObjParseData::posCoords, _r1), qi::_1)]];
-				normal = "vn" >> repeat(3)[double_[phx::push_back(phx::bind(&ObjParseData::normCoords, _r1), qi::_1)]
-					| eps[phx::push_back(phx::bind(&ObjParseData::normCoords, _r1), 0.0f)]];
-				texture = "vt" >> repeat(3)[double_[phx::push_back(phx::bind(&ObjParseData::texCoords, _r1), qi::_1)]
-					| eps[phx::push_back(phx::bind(&ObjParseData::texCoords, _r1), 0.0f)]];
+				vertex = -defaultGroup >> 'v' >> qi::repeat(3)[qi::double_[phx::push_back(phx::bind(&ObjParseData::posCoords, qi::_r1), qi::_1)]];
+				normal = "vn" >> qi::repeat(3)[qi::double_[phx::push_back(phx::bind(&ObjParseData::normCoords, qi::_r1), qi::_1)]
+					| qi::eps[phx::push_back(phx::bind(&ObjParseData::normCoords, qi::_r1), 0.0f)]];
+				texture = "vt" >> qi::repeat(3)[qi::double_[phx::push_back(phx::bind(&ObjParseData::texCoords, qi::_r1), qi::_1)]
+					| qi::eps[phx::push_back(phx::bind(&ObjParseData::texCoords, qi::_r1), 0.0f)]];
 
-				auto addPosIdx  = phx::push_back(phx::bind(&FaceData::posIdx, _r1), qi::_1);
-				auto addTexIdx  = phx::push_back(phx::bind(&FaceData::texIdx, _r1), qi::_1);
-				auto addNormIdx = phx::push_back(phx::bind(&FaceData::normIdx, _r1), qi::_1);
+				auto addPosIdx  = phx::push_back(phx::bind(&FaceData::posIdx, qi::_r1), qi::_1);
+				auto addTexIdx  = phx::push_back(phx::bind(&FaceData::texIdx, qi::_r1), qi::_1);
+				auto addNormIdx = phx::push_back(phx::bind(&FaceData::normIdx, qi::_r1), qi::_1);
 
-				auto setGroupName = phx::push_back(phx::bind(&FaceData::groupName, _r1), qi::_1);
-				auto setUsemtl    = phx::push_back(phx::bind(&FaceData::usemtl, _r1), qi::_1);
+				auto setGroupName = phx::push_back(phx::bind(&FaceData::groupName, qi::_r1), qi::_1);
+				auto setUsemtl    = phx::push_back(phx::bind(&FaceData::usemtl, qi::_r1), qi::_1);
 				groupName = "g" >> qi::skip(qi::blank)[*qi::print[setGroupName]];
 				usemtl    = "usemtl" >> qi::skip(qi::blank)[*qi::print[setUsemtl]];
 
-				face = 'f' >> *(int_[addPosIdx]
-								>> no_skip[-lit('/') >> (int_[addTexIdx] | eps[phx::push_back(phx::bind(&FaceData::texIdx, _r1), 0)])
-								>> -lit('/') >> (int_[addNormIdx] | eps[phx::push_back(phx::bind(&FaceData::normIdx, _r1), 0)])]);
+				face = 'f' >> *(qi::int_[addPosIdx]
+								>> qi::no_skip[-qi::lit('/') >> (qi::int_[addTexIdx] | qi::eps[phx::push_back(phx::bind(&FaceData::texIdx, qi::_r1), 0)])
+								>> -qi::lit('/') >> (qi::int_[addNormIdx] | qi::eps[phx::push_back(phx::bind(&FaceData::normIdx, qi::_r1), 0)])]);
 
-				elem     = -groupName(_val) >> -usemtl(_val) >> face(_val);
-				elements = elem[phx::push_back(phx::bind(&ObjParseData::faces, _r1), qi::_1)];
+				elem     = -groupName(qi::_val) >> -usemtl(qi::_val) >> face(qi::_val);
+				elements = elem[phx::push_back(phx::bind(&ObjParseData::faces, qi::_r1), qi::_1)];
 
-				start = *(matlib(_val) | vertex(_val) | normal(_val) | texture(_val) | elements(_val));
+				start = *(matlib(qi::_val) | vertex(qi::_val) | normal(qi::_val) | texture(qi::_val) | elements(qi::_val));
 			}
 
 			qi::rule<std::string::const_iterator, ObjParseData(), Skipper> start;
@@ -212,38 +214,38 @@ namespace kgx
 		{
 			mtlGram() : mtlGram::base_type(start)
 			{
-				skipLine = (lit('#') | lit("illum")) >> qi::skip(qi::blank)[*qi::print];
+				skipLine = (qi::lit('#') | qi::lit("illum")) >> qi::skip(qi::blank)[*qi::print];
 
-				specPwr = "Ns" >> double_[phx::bind(&ObjMatData::specPwr, _r1) = qi::_1];
-				optDens = "Ni" >> double_[phx::bind(&ObjMatData::optDens, _r1) = qi::_1];
-				transD  = 'd'  >> double_[phx::bind(&ObjMatData::trans, _r1) = qi::_1];
-				transTr = "Tr" >> *double_[phx::bind(&ObjMatData::trans, _r1) = 1.0f - qi::_1];
+				specPwr = "Ns" >> qi::double_[phx::bind(&ObjMatData::specPwr, qi::_r1) = qi::_1];
+				optDens = "Ni" >> qi::double_[phx::bind(&ObjMatData::optDens, qi::_r1) = qi::_1];
+				transD  = 'd'  >> qi::double_[phx::bind(&ObjMatData::trans, qi::_r1) = qi::_1];
+				transTr = "Tr" >> *qi::double_[phx::bind(&ObjMatData::trans, qi::_r1) = 1.0f - qi::_1];
 
-				auto addNewmtl   = phx::push_back(phx::bind(&ObjMatData::name, _r1), qi::_1);
-				auto addAmbMap   = phx::push_back(phx::bind(&ObjMatData::ambMap, _r1), qi::_1);
-				auto addDiffMap  = phx::push_back(phx::bind(&ObjMatData::diffMap, _r1), qi::_1);
-				auto addSpecMap  = phx::push_back(phx::bind(&ObjMatData::specMap, _r1), qi::_1);
-				auto addTransMap = phx::push_back(phx::bind(&ObjMatData::transMap, _r1), qi::_1);
-				auto addBumpMap  = phx::push_back(phx::bind(&ObjMatData::bumpMap, _r1), qi::_1);
+				auto addNewmtl   = phx::push_back(phx::bind(&ObjMatData::name, qi::_r1), qi::_1);
+				auto addAmbMap   = phx::push_back(phx::bind(&ObjMatData::ambMap, qi::_r1), qi::_1);
+				auto addDiffMap  = phx::push_back(phx::bind(&ObjMatData::diffMap, qi::_r1), qi::_1);
+				auto addSpecMap  = phx::push_back(phx::bind(&ObjMatData::specMap, qi::_r1), qi::_1);
+				auto addTransMap = phx::push_back(phx::bind(&ObjMatData::transMap, qi::_r1), qi::_1);
+				auto addBumpMap  = phx::push_back(phx::bind(&ObjMatData::bumpMap, qi::_r1), qi::_1);
 
 				newmtl   = "newmtl" >> qi::skip(qi::blank)[*qi::print[addNewmtl]];
-				ambMap   = "map_Ka" >> qi::skip(qi::blank)[*qi::print[addAmbMap]];
-				diffMap  = "map_Kd" >> qi::skip(qi::blank)[*qi::print[addDiffMap]];
-				specMap  = (lit("map_Ks") | lit("map_refl") | lit("map_Ns")) >> qi::skip(qi::blank)[*qi::print[addSpecMap]];
-				transMap = "map_d" >> qi::skip(qi::blank)[*qi::print[addTransMap]];
-				bumpMap  = (lit("map_bump") | lit("bump")) >> eps[phx::resize(phx::bind(&ObjMatData::bumpMap, _r1), 0)]
-					>> lexeme[*(print - iso8859::space)[addBumpMap]] >> qi::skip(qi::blank)[*qi::print];
+				ambMap   = "map_Ka" >> qi::no_skip[*qi::print[addAmbMap]];
+				diffMap  = "map_Kd" >> qi::no_skip[*qi::print[addDiffMap]];
+				specMap  = (qi::lit("map_Ks") | qi::lit("map_refl") | qi::lit("map_Ns")) >> qi::no_skip[*qi::print[addSpecMap]];
+				transMap = "map_d" >> qi::no_skip[*qi::print[addTransMap]];
+				bumpMap  = (qi::lit("map_bump") | qi::lit("bump")) >> qi::eps[phx::resize(phx::bind(&ObjMatData::bumpMap, qi::_r1), 0)]
+								>> qi::no_skip[*(qi::print)[addBumpMap]];
 
-				xmFloat3 = float_ >> float_ >> float_;
-				transFilter = "Tf" >> xmFloat3[phx::bind( &ObjMatData::transFilter, _r1 ) = qi::_1];
-				ambClr      = "Ka" >> xmFloat3[phx::bind( &ObjMatData::ambClr, _r1 ) = qi::_1];
-				diffClr     = "Kd" >> xmFloat3[phx::bind( &ObjMatData::diffClr, _r1 ) = qi::_1];
-				specClr     = "Ks" >> xmFloat3[phx::bind( &ObjMatData::specClr, _r1 ) = qi::_1];
-				emmisClr    = "Ke" >> xmFloat3[phx::bind( &ObjMatData::emmisClr, _r1 ) = qi::_1];
+				xmFloat3 = qi::float_ >> qi::float_ >> qi::float_;
+				transFilter = "Tf" >> xmFloat3[phx::bind( &ObjMatData::transFilter, qi::_r1 ) = qi::_1];
+				ambClr      = "Ka" >> xmFloat3[phx::bind( &ObjMatData::ambClr, qi::_r1 ) = qi::_1];
+				diffClr     = "Kd" >> xmFloat3[phx::bind( &ObjMatData::diffClr, qi::_r1 ) = qi::_1];
+				specClr     = "Ks" >> xmFloat3[phx::bind( &ObjMatData::specClr, qi::_r1 ) = qi::_1];
+				emmisClr    = "Ke" >> xmFloat3[phx::bind( &ObjMatData::emmisClr, qi::_r1 ) = qi::_1];
 
-				material = newmtl(_val) >> *(specPwr(_val) | optDens(_val) | transD(_val) | transTr(_val)
-											 | transFilter(_val) | ambClr(_val) | diffClr(_val) | specClr(_val) | emmisClr(_val)
-											 | ambMap(_val) | diffMap(_val) | specMap(_val) | transMap(_val) | bumpMap(_val) | skipLine);
+				material = newmtl(qi::_val) >> *(specPwr(qi::_val) | optDens(qi::_val) | transD(qi::_val) | transTr(qi::_val)
+												 | transFilter(qi::_val) | ambClr(qi::_val) | diffClr(qi::_val) | specClr(qi::_val) | emmisClr(qi::_val)
+												 | ambMap(qi::_val) | diffMap(qi::_val) | specMap(qi::_val) | transMap(qi::_val) | bumpMap(qi::_val) | skipLine);
 
 				start = *material;
 			}
@@ -327,10 +329,19 @@ namespace kgx
 			std::vector<long int>::const_iterator texIt = i->texIdx.cbegin();
 			std::vector<long int>::const_iterator normIt = i->normIdx.cbegin();
 
+			UINT firstIdx = 0u;
 			VertexData firstVert( max(0, std::abs(*posIt) - 1), max(0, std::abs(*normIt) - 1), max(0, std::abs(*texIt) - 1) );				//OBJ indices start at 1 and we need to start at 0, so idx - 1
-			UINT firstIdx = maxIdx++;
-			verts.insert( verts.end(), std::pair<VertexData, UINT>( firstVert, firstIdx ) );
-			vertIdices.insert( vertIdices.end(), std::pair<UINT, VertexData>( firstIdx, firstVert ) );
+			std::map<VertexData, UINT>::iterator vertIt;
+			vertIt = verts.find( firstVert );
+			if ( vertIt == verts.end() )
+			{
+				firstIdx = maxIdx++;
+				verts.insert( verts.end(), std::pair<VertexData, UINT>( firstVert, firstIdx ) );
+				vertIdices.insert( vertIdices.end(), std::pair<UINT, VertexData>( firstIdx, firstVert ) );
+			} else
+			{
+				firstIdx = vertIt->second;
+			}
 
 			//TODO: add support for concave polygons
 			posIt++; normIt++; texIt++;
@@ -338,9 +349,8 @@ namespace kgx
 			for ( int j = 0; j < faceSize - 2; ++j )					// triangulate convex polygon
 			{
 				// add triangle vertices to unique-vertex list
-				UINT endVertIdx = 0;
+				UINT endVertIdx = 0u;
 				VertexData endVert( max(0, std::abs(*posIt) - 1), max(0, std::abs(*normIt) - 1), max(0, std::abs(*texIt) - 1) );
-				std::map<VertexData, UINT>::iterator vertIt;
 				vertIt = verts.find( endVert );
 				if ( vertIt == verts.end() )
 				{
@@ -352,7 +362,7 @@ namespace kgx
 					endVertIdx = vertIt->second;
 				}
 
-				UINT midVertIdx = 0;
+				UINT midVertIdx = 0u;
 				posIt++; normIt++; texIt++;
 				VertexData midVert( max(0, std::abs(*posIt) - 1), max(0, std::abs(*normIt) - 1), max(0, std::abs(*texIt) - 1) );
 				vertIt = verts.find( midVert );
@@ -397,7 +407,7 @@ namespace kgx
 
 			// texture UVW coordinates
 			kgData.vertices.push_back( objData.texCoords[vIt->second.texIdx * 3] );
-			kgData.vertices.push_back( 1.0f - objData.texCoords[(vIt->second.texIdx * 3) + 1] );
+			kgData.vertices.push_back( 1.0f - objData.texCoords[(vIt->second.texIdx * 3) + 1] );		// flip v-coordinate
 			kgData.vertices.push_back( objData.texCoords[(vIt->second.texIdx * 3) + 2] );
 
 			// normal coordinates
@@ -459,6 +469,10 @@ namespace kgx
 		kgMat.specular.y = objMat.specClr.y;
 		kgMat.specular.z = objMat.specClr.z;
 		kgMat.specular.w = 1.0f;
+
+		kgMat.diffuseMap  = bfs::path(objMat.diffMap).filename().string();
+		kgMat.specularMap = bfs::path(objMat.specMap).filename().string();
+		kgMat.normalMap   = bfs::path(objMat.bumpMap).filename().string();
 	}
 }
 
