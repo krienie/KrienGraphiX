000a860cd048628cfc9acd83571a20454dbfdfdb
Krien <krien.linnenbank@gmail.com>
2026-04-14 17:26:37 +0200

    Recompiled spdlog on MacOS to exclude exceptions
    Also added a Debug build for it

--- external/macos/spdlog-1.17.0/lib/cmake/spdlog/spdlogConfigTargets.cmake
+++ external/macos/spdlog-1.17.0/lib/cmake/spdlog/spdlogConfigTargets.cmake
@@ -59,7 +59,7 @@ endif()
 add_library(spdlog::spdlog STATIC IMPORTED)
 
 set_target_properties(spdlog::spdlog PROPERTIES
-  INTERFACE_COMPILE_DEFINITIONS "SPDLOG_COMPILED_LIB"
+  INTERFACE_COMPILE_DEFINITIONS "SPDLOG_COMPILED_LIB;SPDLOG_NO_EXCEPTIONS;FMT_USE_EXCEPTIONS=0"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_LINK_LIBRARIES "Threads::Threads"
 )
@@ -68,6 +68,7 @@ set_target_properties(spdlog::spdlog PROPERTIES
 add_library(spdlog::spdlog_header_only INTERFACE IMPORTED)
 
 set_target_properties(spdlog::spdlog_header_only PROPERTIES
+  INTERFACE_COMPILE_DEFINITIONS "SPDLOG_NO_EXCEPTIONS"
   INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
   INTERFACE_LINK_LIBRARIES "Threads::Threads"
 )
